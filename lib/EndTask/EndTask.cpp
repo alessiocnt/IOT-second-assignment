eb65e1f8893fb3425963abe7d8f325bd997e5238
Simone Ceredi <simone.ceredi@studio.unibo.it>
2020-11-20 10:47:56 +0100

    Completed EndTask

---
+++ lib/EndTask/EndTask.cpp
@@ -0,0 +1,23 @@
+#include "EndTask.h"
+
+EndTask::EndTask(BlinkTask* blinkTask, Led* led, ReadyTask* readyTask) {
+    this->blinkTask = blinkTask;
+    this->led = led;
+    this->readyTask = readyTask;
+}
+
+void EndTask::init(int period) {
+    Task::init(period);
+    blinkTask->init(period, led, -1);
+}
+
+void EndTask::tick() {
+    if(MsgService.isMsgAvailable()) {
+        Msg* msg = MsgService.receiveMsg();
+        if (msg->getContent() == "end") {
+            blinkTask->setActive(false);
+            this->setActive(false);
+            readyTask->setActive(true);
+        }
+    }
+}
